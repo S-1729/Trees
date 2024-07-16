bb20c0b5c888bff3783ff37330b213286f0b5dd1
Srinivas Manikanta <97425800+S-1729@users.noreply.github.com>
2024-07-16 20:07:16 +0530

    Add files via upload

---
+++ Step-By-Step Directions From a Binary Tree Node to Another.cpp
@@ -0,0 +1,76 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool solve(TreeNode* root,int x,string &str){
+        if(!root)
+            return false;
+        if(root->val==x)
+            return true;
+        str+='L';
+        if(solve(root->left,x,str))
+            return true;
+        str.pop_back();
+        str+='R';
+        if(solve(root->right,x,str))
+            return true;
+        str.pop_back();
+        return false;
+        
+    }
+    string getDirections(TreeNode* root, int startValue, int destValue) {
+        string str1="",str2="";
+        solve(root,startValue,str1);
+        solve(root,destValue,str2);
+        int i=0;
+        while(i<str1.size() && i<str2.size() && str1[i]==str2[i])
+            i++;
+        string ans="";
+        for(int j=i;j<str1.size();j++)
+            ans.push_back('U');
+        for(int j=i;j<str2.size();j++)
+            ans.push_back(str2[j]);
+        return ans;
+    }
+};
+
+/*
+    You are given the root of a binary tree with n nodes. Each node is uniquely assigned a value from 1 to n. You are also given an integer startValue representing the value of the start node s, and a different integer destValue representing the value of the destination node t.
+    Find the shortest path starting from node s and ending at node t. Generate step-by-step directions of such path as a string consisting of only the uppercase letters 'L', 'R', and 'U'. Each letter indicates a specific direction:
+      .'L' means to go from a node to its left child node.
+      .'R' means to go from a node to its right child node.
+      .'U' means to go from a node to its parent node.
+    Return the step-by-step directions of the shortest path from node s to node t.
+    
+    Example 1:
+                       5
+                    /     \
+                  1         2
+                /         /   \
+              3          6     4
+    
+    Input: root = [5,1,2,3,null,6,4], startValue = 3, destValue = 6
+    Output: "UURL"
+    Explanation: The shortest path is: 3 → 1 → 5 → 2 → 6.
+    
+    Example 2:
+                    2
+                  /
+                1
+    
+    Input: root = [2,1], startValue = 2, destValue = 1
+    Output: "L"
+    Explanation: The shortest path is: 2 → 1.
+    
+    T.C : O(n)
+    S.C : O(n)
+*/
